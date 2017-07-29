2a0f717fe2809416ac4e28c876f9550741ef975c
Artem Personal <artem.litchmanov@gmail.com>
2017-07-29 13:24:52 -0400

    even faster sensors

--- maze_module/maze.py
+++ maze_module/maze.py
@@ -5,25 +5,25 @@ ZERO_PADDING = 20
 
 
 class Maze():
-  _maze_array = []
+    _maze_array = []
 
-  def __init__(self, width, height, image):
-    im = Image.open(image)
-    pix = im.load()
-    self.width, self.height = width, height
-    self._maze_array = np.ones([height + ZERO_PADDING * 2, width + ZERO_PADDING * 2], dtype=np.bool)
-    for i in range(height):
-      for j in range(width):
-        val = (pix[j, i] == (0, 0, 0, 255))
-        self._maze_array[i + ZERO_PADDING, j + ZERO_PADDING] = val
+    def __init__(self, width, height, image):
+        im = Image.open(image)
+        pix = im.load()
+        self.width, self.height = width, height
+        self._maze_array = np.ones([height + ZERO_PADDING * 2, width + ZERO_PADDING * 2], dtype=np.bool)
+        for i in range(height):
+            for j in range(width):
+                val = (pix[j, i] == (0, 0, 0, 255))
+                self._maze_array[i + ZERO_PADDING, j + ZERO_PADDING] = val
 
-  def isSolid(self, x, y):
-    x = round(x)
-    y = round(y)
-    return self._maze_array[y, x]
+    def isSolid(self, x, y):
+        x = round(x)
+        y = round(y)
+        return self._maze_array[y + ZERO_PADDING, x + ZERO_PADDING]
 
-  def draw(self, display_surf):
-    for y in range(len(self._maze_array)):
-      for x in range(len(self._maze_array[y])):
-        if self._maze_array[y, x]:
-          display_surf.set_at((x, y), (0, 0, 0))
+    def draw(self, display_surf):
+        for y in range(len(self._maze_array) - ZERO_PADDING * 2):
+            for x in range(len(self._maze_array[y]) - ZERO_PADDING * 2):
+                if self._maze_array[y + ZERO_PADDING, x + ZERO_PADDING]:
+                    display_surf.set_at((x, y), (0, 0, 0))
--- maze_module/play.py
+++ maze_module/play.py
@@ -7,86 +7,86 @@ from maze_module.robot import Robot
 
 END_STEP_NUM = 400
 BEHAVIOUR_SAMPLE_STEP_NUMS = [
-  (i + 1) * END_STEP_NUM // 3 - 1
-  for i in range(3)
+    (i + 1) * END_STEP_NUM // 3 - 1
+    for i in range(3)
 ]
 
 
 class App:
-  windowWidth = 800
-  windowHeight = 600
-  robot = []
-  render = True
+    windowWidth = 800
+    windowHeight = 600
+    robot = []
+    render = True
 
-  def __init__(self, render=True):
-    self._running = True
-    self._display_surf = None
-    self._image_surfs = [None] * 100
-    self.robot = [None] * 100
-    self.goalPos = (400, 500)
-    self.startPos = (400, 200)
-    self.maze = Maze(800, 600, "./maze_module/maze.png")
-    self.render = render
+    def __init__(self, render=True):
+        self._running = True
+        self._display_surf = None
+        self._image_surfs = [None] * 100
+        self.robot = [None] * 100
+        self.goalPos = (400, 500)
+        self.startPos = (400, 200)
+        self.maze = Maze(800, 600, "./maze_module/maze.png")
+        self.render = render
 
-  def reset(self):
-    self.robot = Robot(self.maze)
-    self.robot.x = self.startPos[0]
-    self.robot.y = self.startPos[1]
+    def reset(self):
+        self.robot = Robot(self.maze)
+        self.robot.x = self.startPos[0]
+        self.robot.y = self.startPos[1]
 
-  def on_init(self):
-    if self.render:
-      pygame.init()
-      self._display_surf = pygame.display.set_mode((self.windowWidth, self.windowHeight), pygame.HWSURFACE)
+    def on_init(self):
+        if self.render:
+            pygame.init()
+            self._display_surf = pygame.display.set_mode((self.windowWidth, self.windowHeight), pygame.HWSURFACE)
 
-      pygame.display.set_caption('Evolved neural nets')
-      self._running = True
-      self._image_surf = pygame.image.load("./maze_module/robot.png").convert()
+            pygame.display.set_caption('Evolved neural nets')
+            self._running = True
+            self._image_surf = pygame.image.load("./maze_module/robot.png").convert()
 
-  def on_render(self):
-    self._display_surf.fill((255, 255, 255))
-    self.maze.draw(self._display_surf)
-    self._display_surf.fill((217, 33, 33), ((self.startPos[0] - 10, self.startPos[1] - 10), (20, 20)))
-    self._display_surf.fill((66, 217, 33), ((self.goalPos[0] - 10, self.goalPos[1] - 10), (20, 20)))
-    self._display_surf.blit(self._image_surf, (self.robot.x, self.robot.y))
-    for j in range(len(self.robot.old_places) - 1):
-      x, y = self.robot.old_places[j]
-      x2, y2 = self.robot.old_places[j + 1]
-      x = round(x)
-      y = round(y)
-      x2 = round(x2)
-      y2 = round(y2)
-      pygame.draw.line(self._display_surf, (217, 0, 0), (x, y), (x2, y2))
+    def on_render(self):
+        self._display_surf.fill((255, 255, 255))
+        self.maze.draw(self._display_surf)
+        self._display_surf.fill((217, 33, 33), ((self.startPos[0] - 10, self.startPos[1] - 10), (20, 20)))
+        self._display_surf.fill((66, 217, 33), ((self.goalPos[0] - 10, self.goalPos[1] - 10), (20, 20)))
+        self._display_surf.blit(self._image_surf, (self.robot.x, self.robot.y))
+        for j in range(len(self.robot.old_places) - 1):
+            x, y = self.robot.old_places[j]
+            x2, y2 = self.robot.old_places[j + 1]
+            x = round(x)
+            y = round(y)
+            x2 = round(x2)
+            y2 = round(y2)
+            pygame.draw.line(self._display_surf, (217, 0, 0), (x, y), (x2, y2))
 
-    pygame.display.flip()
+        pygame.display.flip()
 
-  def on_cleanup(self):
-    if self.render:
-      pygame.quit()
-    self.robot = Robot(self.maze)
-    self.robot.x = self.startPos[0]
-    self.robot.y = self.startPos[1]
+    def on_cleanup(self):
+        if self.render:
+            pygame.quit()
+        self.robot = Robot(self.maze)
+        self.robot.x = self.startPos[0]
+        self.robot.y = self.startPos[1]
 
-  def on_execute(self, neuralNet):
-    if not self.on_init():
-      self._running = False
-    behaviour = []
-    for step in range(END_STEP_NUM):
-      if self.render:
-        pygame.event.pump()
-      inputs = self.robot.getSensorValues() + self.robot.getPieValues(*self.goalPos)
-      left_right, fwd_back = neuralNet.activate(inputs)
-      self.robot.rotate(round(left_right))
-      self.robot.move(round(fwd_back))
-      if step in BEHAVIOUR_SAMPLE_STEP_NUMS:
-        behaviour.append((self.robot.x, self.robot.y))
-      if self.render and step % 20 == 0:
-        self.on_render()
-    self.on_cleanup()
-    objective_value = 640 - distance.euclidean((self.robot.x, self.robot.y), self.goalPos)
-    behaviour = np.array(behaviour).reshape([len(BEHAVIOUR_SAMPLE_STEP_NUMS) * 2])
-    return np.concatenate([behaviour, [objective_value]], -1)
+    def on_execute(self, neuralNet):
+        if not self.on_init():
+            self._running = False
+        behaviour = []
+        for step in range(END_STEP_NUM):
+            if self.render:
+                pygame.event.pump()
+            inputs = self.robot.getSensorValues() + self.robot.getPieValues(*self.goalPos)
+            left_right, fwd_back = neuralNet.activate(inputs)
+            self.robot.rotate(round(left_right))
+            self.robot.move(round(fwd_back))
+            if step in BEHAVIOUR_SAMPLE_STEP_NUMS:
+                behaviour.append((self.robot.x, self.robot.y))
+            if self.render and step % 20 == 0:
+                self.on_render()
+        self.on_cleanup()
+        objective_value = 640 - distance.euclidean((self.robot.x, self.robot.y), self.goalPos)
+        behaviour = np.array(behaviour).reshape([len(BEHAVIOUR_SAMPLE_STEP_NUMS) * 2])
+        return np.concatenate([behaviour, [objective_value]], -1)
 
 
 if __name__ == "__main__":
-  theApp = App()
-  theApp.on_execute()
+    theApp = App()
+    theApp.on_execute()
